ea8c5de0391b1d51e7d981e3f65494a4dbb96e10
p455w0rd <p455w0rd@gmail.com>
2017-09-23 09:30:04 -0400

    allow /dank/null to be used in 1.12.2

--- src/main/java/p455w0rd/danknull/init/ModGlobals.java
+++ src/main/java/p455w0rd/danknull/init/ModGlobals.java
@@ -10,7 +10,7 @@ public class ModGlobals {
 	public static final String MODID_PWLIB = "p455w0rdslib";
 
 	public static final String MODID = "danknull";
-	public static final String VERSION = "1.0.3";
+	public static final String VERSION = "1.0.4";
 	public static final String NAME = "/dank/null";
 	public static final String SERVER_PROXY = "p455w0rd.danknull.proxy.CommonProxy";
 	public static final String CLIENT_PROXY = "p455w0rd.danknull.proxy.ClientProxy";
