d000d7bc6c2f9961297e124cc1af9f0a2d384df3
p455w0rd <p455w0rd@gmail.com>
2017-09-27 15:27:28 -0400

    add support for 1.12.2

--- src/main/java/p455w0rd/danknull/DankNull.java
+++ src/main/java/p455w0rd/danknull/DankNull.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import p455w0rd.danknull.init.ModGlobals;
 import p455w0rd.danknull.proxy.CommonProxy;
 
-@Mod(modid = ModGlobals.MODID, name = ModGlobals.NAME, version = ModGlobals.VERSION, dependencies = ModGlobals.DEPENDANCIES, guiFactory = ModGlobals.GUI_FACTORY, acceptedMinecraftVersions = "[1.12,1.12.1]")
+@Mod(modid = ModGlobals.MODID, name = ModGlobals.NAME, version = ModGlobals.VERSION, dependencies = ModGlobals.DEPENDANCIES, guiFactory = ModGlobals.GUI_FACTORY, acceptedMinecraftVersions = "[1.12,1.12.2]")
 public class DankNull {
 
 	@SidedProxy(clientSide = ModGlobals.CLIENT_PROXY, serverSide = ModGlobals.SERVER_PROXY)
