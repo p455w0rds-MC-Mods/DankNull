6108fae0135c34a674baa8e53d7c30b590b6f73c
p455w0rd <p455w0rd@gmail.com>
2017-12-16 13:16:56 -0500

    Fix #25 and clean up model code

--- src/main/java/p455w0rd/danknull/init/ModGlobals.java
+++ src/main/java/p455w0rd/danknull/init/ModGlobals.java
@@ -10,7 +10,7 @@ public class ModGlobals {
 	public static final String MODID_PWLIB = "p455w0rdslib";
 
 	public static final String MODID = "danknull";
-	public static final String VERSION = "1.2.16";
+	public static final String VERSION = "1.2.17";
 	public static final String NAME = "/dank/null";
 	public static final String SERVER_PROXY = "p455w0rd.danknull.proxy.CommonProxy";
 	public static final String CLIENT_PROXY = "p455w0rd.danknull.proxy.ClientProxy";
